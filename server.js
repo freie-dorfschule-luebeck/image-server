0b67395916ef02e52fa4ca0ffbcc48a88ae2a78a
twentythreetea <helimc.minecraft@gmail.com>
2021-03-04 14:34:06 +0000

    wip better img server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const config = require("../nashi/config.json");
+const utility = require("utility");
 const express = require('express');
-const app = express();
-app.listen(4848);
+const app = express(); app.listen(config.services.image_server.port, () => console.log(config.services.image_server.displayname + ' running on ' + config.services.image_server.port));
 
 //next time i do shit for this i have to add file upload x3
 
@@ -9,19 +10,55 @@ app.use(morgan("dev"));
 app.disable('x-powered-by');
 app.set('etag', false);
 
-const path = require('path');
 const fs = require('fs');
-const utility = require("utility");
+const path = require('path');
 const dir = path.join(__dirname, 'public');
 
-const mime = {
-    jpg: 'image/jpeg',
-};
+const mysql = require("mysql2");
+const db_config = {
+    host : config.mysql.host,
+    user : config.mysql.user,
+    password : config.mysql.password,
+    database : config.mysql.database,
+    timezone: config.mysql.timezone,
+    insecureAuth : true
+}
+
+let connection;
+
+handleDisconnect = () => {
+    mysql.connection = mysql.createConnection(db_config);
+    mysql.connection.connect(function(err) {
+        if(err) {
+            console.log('MYSQL CONNECT ERR ' + err);
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log(`Successfully connected to MYSQL`)
+        }
+    });
+
+    mysql.connection.on('error', function(err) {
+        console.log('MYSQL ERR ' + err);
+        handleDisconnect();                         
+    });
+}
+
+handleDisconnect();
 
 try {
     app.all('/', (req, res, next) => {
-        res.redirect('https://github.com/fdl-stuff/image-server');
+        res.set('Content-Type', 'application/json');
+        body = {
+            code: 200,
+            message: "the image-server is working ^_^"
+        };
+        res.statusMessage = utility.errorHandling.ErrorStatusCodes[body.code];
+        res.status(body.code).send(body)
     })
+    app.get("/:type/:type_id", (req, res, next) => {
+        next();
+    });
+
     app.get('*', function (req, res, next) {
         let file = path.join(dir, req.path);
         // let extname = path.extname(file).slice(1);
@@ -31,21 +68,24 @@ try {
             stream.pipe(res);
         });
         stream.on('error', function () {
-            res.sendFile(path.join(dir, '/users/0.svg'));
+           stream.close();
+           next();
         });
     });
 } catch (err) {
+    console.log(err)
     app.use((req, res, next) => next(err));
 }
 
-app.use((req, res, next) => next(new utility.errorHandling.SutekinaStatusError(404)));
+// 
+app.use((req, res, next) => res.status(404).sendFile(path.join(dir, '/0.svg')));
 
 app.use((err, req, res, next) => {
-    res.set('Content-Type', 'text/plain');
+    res.set('Content-Type', 'application/json');
     body = {
         code: err.status || err.statusCode || 500,
         message: err.message || err
     };
     res.statusMessage = utility.errorHandling.ErrorStatusCodes[body.code];
-    res.status(body.code).send(body.message)
+    res.status(body.code).send(body)
 });
