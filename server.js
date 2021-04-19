6e4534208bad6dce67c590d6342064bbf193e599
BuildTools <unconfigured@null.spigotmc.org>
2021-04-19 05:35:37 +0000

    added image uploading still wip there are still lots of thigns i have to consider rn in theory peole can upload eeveryhitg i need to check the mimetype and stuff yup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const config = require("../nashi/config.json");
 const constants = require("./constants");
-const utility = require("utility");
+const utility = require("../nashi/utility");
 const express = require('express');
 const expressSession = require('express-session');
 const sharp = require('sharp');
+const multer = require("multer");
 const app = express(); app.listen(config.services.image_server.port, () => console.log(config.services.image_server.displayname + ' running on ' + config.services.image_server.port));
 
 //next time i do shit for this i have to add file upload x3
@@ -48,6 +49,28 @@ handleDisconnect = () => {
 }
 
 handleDisconnect();
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, newdir);
+    },
+    filename: (req, file, callback) => {
+        mysql.connection.execute(`INSERT INTO ringo.images (user_id, type_id, type, file_format, hidden, created_at) VALUES (?, ?, ?, ?, "0", UTC_TIMESTAMP())`, [req.session.user_id, req.session.user_id, "user", file.originalname.split('.').pop()], (error, result) => {
+            if(error) return callback(new utility.errorHandling.SutekinaError(error.message, 500));
+            return callback(null, `${result.insertId}.${file.originalname.split('.').pop()}`);
+        });
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fields: 10,
+        fileSize: 1024 * 1024 * 1024, // 1GB, before this is possible nginx will block.
+        files: 1        
+    }
+});
 
 const MySQLStore = require('express-mysql-session')(expressSession);
 
@@ -96,24 +119,66 @@ try {
         res.status(body.code).send(body)
     })
     app.get("/404.banner", (req, res, next) => {
-        if(!req.query.new) return next();
         let file = path.join(newdir, "banner.png");
         res.sendFile(file);
     });
     app.get("/:type/:type_id", (req, res, next) => {
-        if(!req.query.new) return next();
-
         let type = req.params.type;
         let type_id = req.params.type_id;
 
         mysql.connection.execute(`SELECT * FROM images WHERE type = ? AND type_id = ? AND hidden = 0 ORDER BY image_id DESC`, [type, type_id], (error, result) => {
             if(error) return next(error);
             if(!result[0]) return next();
-            res.redirect(302, `/${result[0].image_id}.${result[0].file_format}?new=true&size=${req.query.size || "max"}`);
+            res.redirect(302, `/${result[0].image_id}.${result[0].file_format}?size=${req.query.size || "max"}`);
+        });
+    });
+    app.post("/:type/:type_id", (req, res, next) => {
+        if(!req.session.nick) {
+            req.session.error = {
+                message: "Du bist nicht angemeldet.",
+                status: 401
+            };
+            return res.redirect(`${config.services.nashi.domain}/error`);
+        }
+        upload.single("file")(req, res, (err) => {
+            if (err) {
+                req.session.error = {
+                    message: err.message,
+                    status: err.status || 500
+                };
+                console.log(err);
+                res.redirect(`${config.services.nashi.domain}${req.session.redir || "/error"}`);
+            } else {
+                if(req.file.size > (1 * 1024 * 1024)) {
+                    fs.readFile(req.file.path, (err, data) => {
+                        if(err) {
+                            req.session.error = {
+                                message: err.message,
+                                status: err.status || 500
+                            };
+                            console.log(err);
+                            res.redirect(`${config.services.nashi.domain}${req.session.redir || "/error"}`);
+                        } else {
+                            sharp(data, { failOnError: false, withoutEnlargement: true }).resize(2000).toFile(req.file.path).then(info => {
+                                console.log(info);
+                                res.redirect(`${config.services.nashi.domain}${req.session.redir || ""}`);
+                            }).catch(err => {
+                                req.session.error = {
+                                    message: err.message,
+                                    status: err.status || 500
+                                };
+                                console.log(err);
+                                res.redirect(`${config.services.nashi.domain}${req.session.redir || "/error"}`);
+                            });
+                        }
+                    });
+                } else {
+                    res.redirect(`${config.services.nashi.domain}${req.session.redir || ""}`);
+                }
+            }
         });
     });
     app.get("/:path", (req, res, next) => {
-        if(!req.query.new) return next();
         let file = path.join(newdir, req.params.path);
         if(!path.extname(file).slice(1)) return next();
     
@@ -124,8 +189,8 @@ try {
         mysql.connection.execute(`SELECT * FROM images WHERE image_id = ? AND hidden = 0`, [image_id], (error, result) => {
             if(error) return next(error);
             if(!result[0]) return next();
-            if(file_format !== result[0].file_format) return res.redirect(301, `/${image_id}.${result[0].file_format}?new=true`);
-            sharp(file).resize(file_size).toBuffer().then(data => res.end(data)).catch(err => next(err));
+            if(file_format !== result[0].file_format) return res.redirect(301, `/${image_id}.${result[0].file_format}`);
+            sharp(file, { failOnError: false }).resize(file_size).toBuffer().then(data => res.end(data)).catch(err => next(err));
         });
     });
 } catch (err) {
